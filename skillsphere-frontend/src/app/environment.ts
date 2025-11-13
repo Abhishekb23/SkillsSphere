47cd63ae48e0f0b4a983342c1b18c5da5d0b47ed
Abhishek Barman <114233335+Abhishekb23@users.noreply.github.com>
2025-11-14 01:34:51 +0530

    Update environment.ts

--- skillsphere-frontend/src/app/environment.ts
+++ skillsphere-frontend/src/app/environment.ts
@@ -1,8 +1,8 @@
 const environment = {
   production: false,
 
-  BASE_URL: 'http://localhost:8080/api',
-  // BASE_URL: 'https://skillssphere-1.onrender.com/api',
+ // BASE_URL: 'http://localhost:8080/api',
+  BASE_URL: 'https://skillssphere-1.onrender.com/api',
 };
 
 export default environment;
